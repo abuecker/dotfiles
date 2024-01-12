60f1cdf571f83059ab9d47ed648628eb2a388db4
Andy Buecker <andy@48s.io>
2024-01-12 14:06:31 -0800

    add support for Dracula theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -68,6 +68,17 @@ local lsp_servers = {
 }
 
 require("lazy").setup({
+	-- {
+	-- 	"dracula/vim",
+	-- 	name = "dracula",
+	-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	-- 	priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	config = function()
+	-- 		-- load the colorscheme here
+	-- 		vim.cmd("colorscheme dracula")
+	-- 	end,
+	-- },
+
 	-- Rose Pine theme
 	{
 		"rose-pine/neovim",
